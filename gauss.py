50b25e081d105f3f1e64c379720ffb9a33afd9a5
farooq-azam-khan <khanazamfarooq99@gmail.com>
2023-01-05 15:57:53 -0500

    experimenting with anomaly detection

--- gauss.py
+++ gauss.py
@@ -9,8 +9,32 @@ def gauss_anom(dataset):
         var[i] = np.sum((dataset[:, i] - mu[i])**2) / examples 
     return mu, var 
 
-dataset = np.array([[1,2,3], [1.1, 2, 3.5], [1.15, 2.25, 3], [1.001, 2.002, 3.001]])
-print(dataset)
+dataset = np.array([[1,2,3], 
+    [1.1, 2, 3.5], 
+    [1.1, 2, 3.5], 
+    [1.1, 2, 3.5], 
+    [1.1, 2, 3.5], 
+    [1.1, 2, 3.5], 
+    [1.1, 2, 3.5], 
+    [1.1, 2, 3.5], 
+    [1.1, 2, 3.5], 
+    [1.1, 2, 3.5], 
+    [1.1, 2, 3.5], 
+    [1.1, 2, 3.5], 
+    [1.1, 2, 3.5], 
+    [1.1, 2, 3.5], 
+    [1.1, 2, 3.5], 
+    [1.1, 2, 3.5], 
+    [1.1, 2, 3.5], 
+    [1.1, 2, 3.5], 
+    [1.1, 2, 3.5], 
+    [1.1, 2, 3.5], 
+    [1.15, 2.25, 3], 
+    [1.15, 2.25, 3], 
+    [1.15, 2.25, 3], 
+    [1.15, 2.25, 3], 
+    [1.15, 2.25, 3], 
+    [1.001, 2.002, 3.001]])
 mu, var = gauss_anom(dataset) 
 print('mu=')
 print(mu)
@@ -25,11 +49,20 @@ def compute_p(new_datapoint, mu, var):
     for x, m, s in zip(new_datapoint, mu, var): 
         coef = 1/(np.sqrt(2*np.pi)*s)
         prob *= coef * np.exp((-1*(x-m)**2)/(2*s*s))
+
+    coeff1 = 1/(np.sqrt(2*np.pi)*var)
+    coeff2 = np.exp(-1 * (new_datapoint-mu)**2 / (2*var*var))
+    print( coeff1  * coeff2 )
     return prob 
-threashold = 0.01
+threashold = 0.001
 prob = compute_p(new_datapoint, mu, var) 
-print(f'prob(new_datapoint) = {prob}')
-if prob > threashold:  
-    print(f'Not an Anomaly')
-else: 
-    print('Anomaly')
+def is_anomaly(threashold, prob): 
+    print(f'prob(point) = {prob}')
+    if prob > threashold:  
+        print(f'Not an Anomaly')
+    else: 
+        print('Anomaly')
+
+is_anomaly(threashold, prob)
+prob = compute_p(np.array([1.1, 2, 3.5]), mu, var)
+is_anomaly(threashold, prob)
